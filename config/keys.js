88d93ae524edac7eee1203458909c29e3e8758f7
Anshuman Saha <anshumansaha99@yahoo.com>
2021-03-07 15:09:21 +0530

    Update keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb+srv://anshuman:Anshu@1234@cluster0.rtdwn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  mongoURI: "",
   secretOrKey: "secret",
 };
